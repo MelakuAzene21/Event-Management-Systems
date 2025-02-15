f93ce935de1a5279348980363a668fcc0adceb45
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-02-15 09:06:10 +0300

    Add:add Booking Model

---
+++ back/models/Booking.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema(
+   {
+        user : { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true }, // Reference to the user booking the event
+        event: { type: mongoose.Schema.Types.ObjectId,
+         ref: 'Event', 
+         required: true }, // Reference to the event being booked
+        ticketType: { type: String,
+         required: true }, // e.g., "VIP", "Regular", "Student"
+        ticketCount: { type: Number, 
+        required: true }, // Number of tickets
+       ticktAmount: { type: Number, 
+        required: true }, // Total amount of the tickets
+       paymentId: { type: String, required: true }, // Payment ID from the payment gateway
+       status: { type: String, default:'booked'}, // Booking status, e.g., "Pending", "Confirmed", "Cancelled"
+
+}
+
+)
+module.exports = mongoose.model('Booking', BookingSchema);
