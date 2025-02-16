9ab67852790974cc07d84a865330a6192ba9d812
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-02-16 21:42:48 +0300

    /fix:fix on boonking model

--- back/routes/bookingRoutes.js
+++ back/routes/bookingRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middlewares/verifyToken'); // Adjust the path
-const Booking = require('../models/Booking'); // Adjust the path    
-const Event = require('../models/Event'); // Adjust the path
+const verifyToken = require('../middlewares/verifyToken');
+const Booking = require('../models/Booking');    
+const Event = require('../models/Event');
 
 router.post("/create-booking", verifyToken, async (req, res) => {
 
@@ -13,23 +13,23 @@ try {
 
     //update event tickets
     const event = await Event.findById(req.body.event);
-    const ticketType=event.ticketTypes
+    const ticketType=event.ticketTypes;
 
     const updatedTicketsTypes = ticketType.map(ticketType => {
         if (ticketType.name === req.body.ticketType) {
             ticketType.booked=Number(ticketType.booked ?? 0)+Number(req.body.ticketsCount);
-        ticketType.avaliable=Number(ticketType.avaliabale ?? ticketType.limit)-Number(express.request.body.ticketsCount)
+            ticketType.available = Number(ticketType.available ?? ticketType.limit) - Number(req.body.ticketsCount)
         }
         return ticketType;
     });
     await Event.findByIdAndUpdate(req.body.event,{
         ticketTypes: updatedTicketsTypes,
 
-    })
+    });
 return res.status(201).json({message:"Booking Successfuuly",booking})
 }catch (error) {
 
     return res.status(500).json({message:error.message})
 }
 })
-model.exports = router;
+module.exports = router;
