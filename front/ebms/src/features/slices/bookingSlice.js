7a4d5fda2f5797695f87a3348d741f9d1fe2fe31
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-02-19 21:53:07 +0300

    add redux Persistent

--- front/ebms/src/features/slices/bookingSlice.js
+++ front/ebms/src/features/slices/bookingSlice.js
@@ -3,10 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const bookingSlice = createSlice({
     name: "booking",
     initialState: {
+        pendingBooking: null, // Store booking details before payment
         currentBooking: null, // Store the latest booking
         bookings: [], // Store all user bookings
     },
     reducers: {
+        setPendingBooking: (state, action) => {
+            state.pendingBooking = action.payload; // Store booking details temporarily
+        },
+        clearPendingBooking: (state) => {
+            state.pendingBooking = null; // Clear after payment
+        },
         setCurrentBooking: (state, action) => {
             state.currentBooking = action.payload;
         },
@@ -16,5 +23,5 @@ const bookingSlice = createSlice({
     },
 });
 
-export const { setCurrentBooking, setUserBookings } = bookingSlice.actions;
+export const { setCurrentBooking, setUserBookings ,clearPendingBooking,setPendingBooking} = bookingSlice.actions;
 export default bookingSlice.reducer;
--- front/ebms/src/index.js
+++ front/ebms/src/index.js
@@ -21,15 +21,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store ,persistor} from './store/store';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
-  
+    </PersistGate>
+  </Provider>
 );
