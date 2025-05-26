7cbf491baf5be878d68f752570cc5c1f2b7db3bd
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-05-26 12:16:21 +0300

    share event to social media

--- front/ebms/src/UserPage/ShareEvent.js
+++ front/ebms/src/UserPage/ShareEvent.js
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTitle } from "../components/ui/dialog";
-import { FaFacebook, FaTwitter, FaLinkedin, FaWhatsapp, FaTelegram, FaShareAlt } from "react-icons/fa";
+import { FaFacebook, FaTwitter,  FaWhatsapp, FaTelegram, FaShareAlt } from "react-icons/fa";
 
 const ShareEventModal = ({ event }) => {
     const { title, _id } = event;
 
-    const eventUrl = `https://event-hub-vercel.vercel.app/${_id}`;
+    const eventUrl = `https://event-hub-vercel.vercel.app/events/${_id}`;
 
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false); 
 
     const socialLinks = {
         facebook: `https://www.facebook.com/sharer/sharer.php?u=${eventUrl}`,
         twitter: `https://twitter.com/intent/tweet?url=${eventUrl}&text=${title}`,
-        linkedin: `https://www.linkedin.com/shareArticle?url=${eventUrl}&title=${title}`,
         whatsapp: `https://api.whatsapp.com/send?text=${title}%20${eventUrl}`,
         telegram: `https://t.me/share/url?url=${eventUrl}&text=${title}`,
     };
@@ -36,9 +35,7 @@ const ShareEventModal = ({ event }) => {
                         <a href={socialLinks.twitter} target="_blank" rel="noopener noreferrer">
                             <FaTwitter size={32} className="text-blue-400 hover:text-blue-600" />
                         </a>
-                        <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
-                            <FaLinkedin size={32} className="text-blue-500 hover:text-blue-700" />
-                        </a>
+                        
                         <a href={socialLinks.whatsapp} target="_blank" rel="noopener noreferrer">
                             <FaWhatsapp size={32} className="text-green-500 hover:text-green-700" />
                         </a>
